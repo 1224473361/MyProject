11b311a31055d1abfb085503692eaa57ba0d19b1
lihui <liuhui@95081.com>
2019-06-13 15:17:54 +0800

    优化excel工具类

--- src/main/java/com/xhx/poi/controller/GenerateExcelViewController.java
+++ src/main/java/com/xhx/poi/controller/GenerateExcelViewController.java
@@ -12,12 +12,15 @@ import com.xhx.common.util.ECollectionUtil;
 import com.xhx.poi.util.EExcelUtil;
 import com.xhx.poi.view.EExcelView;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 
  * @date 2019年5月30日
  * @author lihui
  */
 @Controller
+@Slf4j
 public class GenerateExcelViewController {
 
 	/**
@@ -25,24 +28,33 @@ public class GenerateExcelViewController {
 	 * 
 	 * @return
 	 */
-	@RequestMapping("/exportOrgExcel")
+	@RequestMapping("/exportXExcel")
 	public ModelAndView generateOrgExcel() {
+		long time1 = System.currentTimeMillis();
 		String[] headers = { "序号", "机构编号", "组织名称", "组织级别", "所处组织链", "领导账户", "领导级别" };
 		String[] keys = { "index", "prop1", "prop2", "prop3", "prop4", "prop5", "prop6" };
 		List<Map<String, String>> datas = ECollectionUtil.getList();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 100*10000; i++) {
 			Map<String, String> m = ECollectionUtil.getMap();
-			m.put(keys[0], i + "");
-			for (int j = 0; j < 6 - 2; j++) {
+			m.put(keys[0], (i + 1) + "");
+			for (int j = 1; j < keys.length; j++) {
 				m.put(keys[j], "prop-" + j + "-" + i);
 			}
 			datas.add(m);
 		}
+		long time2 = System.currentTimeMillis();
+		log.info("数据准备时间:{}，数据量:{}", (time2 - time1),datas.size());
 		String filename = "测试.xlsx";
-		Workbook wb = EExcelUtil.getWorkBook(headers, keys, datas, filename);
+		Workbook wb = EExcelUtil.getWorkBook(headers, keys, datas, filename,true);
 		Map<String, Object> model = ECollectionUtil.getMap();
 		model.put("filename", filename);
 		model.put("workbook", wb);
+		long time3 = System.currentTimeMillis();
+		log.info("Excel生成时间：{}", (time3 - time2));
+		log.info("Excel生成总耗时：{}", (time3 - time1));
 		return new ModelAndView(new EExcelView(), model);
 	}
+	
+	
+	
 }
--- src/main/java/com/xhx/poi/util/EExcelUtil.java
+++ src/main/java/com/xhx/poi/util/EExcelUtil.java
@@ -6,11 +6,15 @@ import java.util.Map;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
@@ -24,13 +28,46 @@ public class EExcelUtil {
 	public static final String PREFIX_XLS = "xls";
 	public static final String PREFIX_XLSX = "xlsx";
 
+	private static String getSuffix(String fileName) {
+		return fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
+	}
+
+	/**
+	 * 可以根据名字自动匹配xlsx和xls格式
+	 * 
+	 * @param headers
+	 * @param keys
+	 * @param datas
+	 * @param fileName
+	 * @return
+	 */
 	public static Workbook getWorkBook(String[] headers, String[] keys, List<Map<String, String>> datas,
 			String fileName) {
+		return getWorkBook(headers, keys, datas, fileName, false);
+	}
+
+	/**
+	 * 可以根据名字自动匹配xlsx和xls格式
+	 * 
+	 * @param headers
+	 * @param keys
+	 * @param datas
+	 * @param fileName
+	 * @param isBigData 当数据量超过65535时，需使用SXSSFWorkbook
+	 * @return
+	 */
+	public static Workbook getWorkBook(String[] headers, String[] keys, List<Map<String, String>> datas,
+			String fileName, boolean isBigData) {
 		Workbook wb = null;
-		if (fileName.contains(PREFIX_XLS)) {
+		if (getSuffix(fileName).equals(PREFIX_XLSX)) {
+			if (isBigData) {
+				//这样表示SXSSFWorkbook只会保留200条数据在内存中，其它的数据都会写到磁盘里，这样的话占用的内存就会很少
+				wb = new SXSSFWorkbook(new XSSFWorkbook(), 200);
+			} else {
+				wb = new XSSFWorkbook();
+			}
+		} else if (getSuffix(fileName).equals(PREFIX_XLS)) {
 			wb = new HSSFWorkbook();
-		} else if (fileName.contains(PREFIX_XLSX)) {
-			wb = new XSSFWorkbook();
 		} else {
 			return null;
 		}
@@ -39,10 +76,11 @@ public class EExcelUtil {
 		Row row = sheet.createRow(0);
 		CellStyle cellStyle = wb.createCellStyle();
 		// 居中样式
-		cellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 		Font font2 = wb.createFont();
-		// font2.setFontName("仿宋_GB2312");
-		font2.setBoldweight(Font.BOLDWEIGHT_BOLD);// 粗体显示
+		// 粗体显示
+		font2.setBold(true);
 		cellStyle.setFont(font2);// 选择需要用到的字体格式
 
 		// 表头
@@ -58,7 +96,8 @@ public class EExcelUtil {
 		DataFormat format = wb.createDataFormat();
 		ccellStyle.setDataFormat(format.getFormat("@"));
 		// 居中样式
-		ccellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		ccellStyle.setAlignment(HorizontalAlignment.CENTER);
+		ccellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 
 		Row crow = null;
 		for (int i = 0; i < datas.size(); i++) {
@@ -68,7 +107,7 @@ public class EExcelUtil {
 				cell = crow.createCell(j);
 				cell.setCellValue(map.get(keys[j]));
 				cell.setCellStyle(ccellStyle);
-				cell.setCellType(Cell.CELL_TYPE_STRING);
+				cell.setCellType(CellType.STRING);
 			}
 		}
 		return wb;
