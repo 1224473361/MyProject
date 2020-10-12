7d2c8697a124084ca958fb8660cbaeb6e64d42f8
lihui <1224473361@qq.com>
2020-10-12 16:18:01 +0800

    调整resttemplate生成逻辑

--- src/main/java/com/xhx/restclient/client/MHttpClient.java
+++ src/main/java/com/xhx/restclient/client/MHttpClient.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSONObject;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 模拟http请求
@@ -16,10 +22,14 @@ import org.springframework.web.client.RestTemplate;
  * @author lihui
  */
 @Service
+@Slf4j
 public class MHttpClient {
 
 	@Autowired
-	RestTemplate template;
+	private RestTemplate template;
+	private static final String KEY_AUTHORIZATION = "Authorization";
+	private static final String KEY_BEARER = "Bearer ";
+	private static final String LOG_STR = "{}:{}";
 
 	/**
 	 * 发送http请求
@@ -34,9 +44,98 @@ public class MHttpClient {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED);
 		// 组装
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers); 
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
 		ResponseEntity<String> response = template.exchange(url, method, request, String.class, params);
 		return response.getBody();
+	}
+
+	/**
+	 * 连接url
+	 * 
+	 * @param url
+	 * @param obj
+	 * @return
+	 */
+	public JSONObject restToPost(String url, JSONObject obj) {
+		// header参数
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(KEY_AUTHORIZATION, KEY_BEARER + obj.getString("token"));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		try {
+			// 放入body中的json参数 假如有其他参数可以放置进去
+			HttpEntity<JSONObject> request = new HttpEntity<>(obj, headers);
+			log.info(LOG_STR, url, "开始请求。。。");
+			ResponseEntity<JSONObject> responseEntity = template.exchange(url, HttpMethod.POST, request,
+					JSONObject.class);
+			log.info(LOG_STR, url, responseEntity.getBody());
+			return responseEntity.getBody();
+		} catch (ResourceAccessException e) {
+			log.info("[{}]", e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	/**
+	 * 
+	 * @param url
+	 * @param paramMap
+	 * @param token
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public JSONObject restToFromDataPost(String url, MultiValueMap<String, String> paramMap, String token) {
+		// header参数
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(KEY_AUTHORIZATION, KEY_BEARER + token);
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		try {
+			// 2、使用postForEntity请求接口
+			org.springframework.http.HttpEntity<MultiValueMap<String, Object>> request = new org.springframework.http.HttpEntity(
+					paramMap, headers);
+			ResponseEntity<JSONObject> responseEntity = template.postForEntity(url, request, JSONObject.class);
+			log.info(LOG_STR, url, responseEntity.getBody());
+			return responseEntity.getBody();
+		} catch (ResourceAccessException e) {
+			log.info("[{}]", e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return null;
+		}
+	}
+
+	/**
+	 * 使用template调用外部rest接口<br>
+	 * 需要往headers里面存入Authorization:Bearer token
+	 *
+	 * @param url   地址
+	 * @param obj   参数
+	 * @param token token
+	 * @return
+	 */
+	public JSONObject restToPost(String url, JSONObject obj, String token) {
+		// header参数
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(KEY_AUTHORIZATION, KEY_BEARER + token);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		try {
+			// 放入body中的json参数 假如有其他参数可以放置进去
+			HttpEntity<JSONObject> request = new HttpEntity<>(obj, headers);
+			log.info(LOG_STR, url, "开始请求。。。");
+			ResponseEntity<JSONObject> responseEntity = template.exchange(url, HttpMethod.POST, request,
+					JSONObject.class);
+			log.info(LOG_STR, url, responseEntity.getBody());
+			return responseEntity.getBody();
+		} catch (ResourceAccessException e) {
+			log.info("[{}]", e.getMessage());
+			throw e;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return null;
+		}
 	}
 
 }
--- src/main/java/com/xhx/restclient/config/RestConfig.java
+++ src/main/java/com/xhx/restclient/config/RestConfig.java
@@ -1,8 +1,19 @@
 package com.xhx.restclient.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -15,7 +26,46 @@ import org.springframework.web.client.RestTemplate;
 public class RestConfig {
 
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+	@Primary
+	public RestTemplate httpRestTemplate() {
+		// restTemplate.setMessageConverters(...); 可以添加消息转换
+		// restTemplate.setInterceptors(...); 可以增加拦截器
+		return new RestTemplate(httpRequestFactory());
+	}
+
+	private ClientHttpRequestFactory httpRequestFactory() {
+		return new HttpComponentsClientHttpRequestFactory(restTemplateConfigHttpClient());
+	}
+
+	/**
+	 * 配置httpclient连接池
+	 * 
+	 * @return
+	 */
+	private HttpClient restTemplateConfigHttpClient() {
+		Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("http", PlainConnectionSocketFactory.getSocketFactory())
+				.register("https", SSLConnectionSocketFactory.getSocketFactory()).build();
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+		// 设置整个连接池最大连接数 根据自己的场景决定
+		// 后面调整从配置中心获取
+		connectionManager.setMaxTotal(50);
+		// 路由是对maxTotal的细分
+		// 后面调整从配置中心获取
+		connectionManager.setDefaultMaxPerRoute(100);
+		RequestConfig requestConfig = RequestConfig.custom()
+				// 服务器返回数据(response)的时间，超过该时间抛出read timeout
+				// 后面调整从配置中心获取
+				.setSocketTimeout(10000)
+				// 连接上服务器(握手成功)的时间，超出该时间抛出connect timeout
+				// 后面调整从配置中心获取
+				.setConnectTimeout(5000)
+				// 从连接池中获取连接的超时时间，超过该时间未拿到可用连接，
+				// 会抛出org.apache.http.conn.ConnectionPoolTimeoutException:
+				// Timeout waiting for connection from pool
+				// 后面调整从配置中心获取
+				.setConnectionRequestTimeout(5000).build();
+		return HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).setConnectionManager(connectionManager)
+				.build();
 	}
 }
